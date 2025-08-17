08394bb08788f2f02eb688fcf4cefbd8c18bef9a
TumasUCS <luidherodrigues@gmail.com>
2025-08-17 19:08:26 -0300

    exercicio032.py

--- exercicios/exercicio032.py
+++ exercicios/exercicio032.py
@@ -1,5 +1,8 @@
-ano = int(input('digite o ano: '))
-if ano % 4 == 0 and ano < 2025:
+from datetime import date
+ano = int(input('digite o ano que voce quer analisar e digite 0 para analisar o ano atual: '))
+if ano ==0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 !=0 and ano < 2025:
     print(f'{ano} foi bissexto')
 elif ano % 4 == 0 and ano >= 2025:
     print(f'{ano} vai ser bissexto')
@@ -7,3 +10,4 @@ elif ano % 4 != 0 and ano >= 2026:
     print(f'{ano} nao vai ser bissexto')
 else:
     print(f'{ano} nao foi bissexto')
+    
